867c242c7e1ad1ac0376f6a252e3abb40634574c
Cole Bowers <cole.a.bowers@gmail.com>
2017-10-14 08:05:45 -0400

    document template

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-A
-B
-C
-D
+var populateCodes = function() {
+	document.getElementById("airports").innerHTML += "<option value='HI'>";
+
+	$.getJSON("airports.json", function( json ) {  // Missing this curly brace.
+  		$.each(json, function(key, value) {
+    		// Change key and json[key] to json[key].value and json[key].title
+    		console.log(key);
+    		console.log(value);
+    		$('select[name=cars]').append('<option value="' + json[key].value + '">' + json[key].name + ' ('+ json[key].iata + ')' + '</option>');
+  		}); 
+	});
+}
