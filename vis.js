6466b9626ad6bfc49e97860f548e786ef23e26ff
Nancy Tao <nancy.tao42@gmail.com>
2017-10-24 21:20:55 -0400

    Update vis.js

--- vis.js
+++ vis.js
@@ -1,8 +1,4 @@
-// <<<<<<< HEAD
-// //set the margins and labels of the graph. 
-// =======
-// // full heat map
-// >>>>>>> 605f76f04a3cd459b9723c573815b33bcfccd46f
+// set the margins and labels of the graph. 
 var margin = { top: 50, right: 0, bottom: 50, left: 30 },
     height = 600 - margin.top - margin.bottom,
     width = 430 * 1.5 - margin.left - margin.right,
@@ -12,22 +8,22 @@ var margin = { top: 50, right: 0, bottom: 50, left: 30 },
     days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     times = ["12a", "1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a",
              "12p", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p"];
-//temporary hardcode the flight time 
+// temporary hardcode the flight time 
 var flight_day = 5 ;//tuesday; 
-var flight_time = "12:00pm"; //note: starting with base case that requires no wraparound 
-//get the hour from the flight time and convert it to an int -- might want to do a check that it's a valid integer
+var flight_time = "12:00pm"; // note: starting with base case that requires no wraparound 
+// get the hour from the flight time and convert it to an int -- might want to do a check that it's a valid integer
 var flight_hour = 12;// Number(flight_time.substring(0,2)); 
 // console.log(flight_hour);
 
 
-//create the chart from d3 and add it to the general container 
+// create the chart from d3 and add it to the general container 
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right) // set margins
     .attr("height", height + margin.top + margin.bottom) 
     .append("g") //append the graph portion ?
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); //keep the graph within the margins?
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); // keep the graph within the margins?
 
-//create a tooltip that when hovered over, shows the day of the week, the time, and the wait time in minutes 
+// create a tooltip that when hovered over, shows the day of the week, the time, and the wait time in minutes 
 var tooltip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
@@ -35,7 +31,7 @@ var tooltip = d3.tip()
     return days[d.day-1] + " at " + times[d.hour-1] + ": " + d.value + " min";
   });
 
-//create the daylabels on the heatmap --> eventually should only display the day labels that we want to see?
+// create the daylabels on the heatmap --> eventually should only display the day labels that we want to see?
 var dayLabels = svg.selectAll(".dayLabel")
     .data(days)
     .enter().append("text")
@@ -49,7 +45,8 @@ var dayLabels = svg.selectAll(".dayLabel")
                                 "dayLabel mono axis axis-workweek" :
                                 "dayLabel mono axis");
                      });
-//time labels. get to only dispaly the time labels that we need. 
+
+// time labels. get to only dispaly the time labels that we need. 
 var timeLabels = svg.selectAll(".timeLabel")
       .data(times)
       .enter().append("text")
@@ -104,16 +101,10 @@ d3.csv('data.csv',
 //             return (d.hour - 1) * gridSize;
 //           }
 //           })
-//         // .attr("rx", 4)
-//         // .attr("ry", 4)
-//         // .attr("class", "hour bordered")
-// =======
-        .attr("x", function(d) { return (d.day - 1) * gridSize * 3.2; })
-        .attr("y", function(d) { return (d.hour - 1) * gridSize})
-        .attr("rx", 4)
-        .attr("ry", 4)
-        .attr("class", "hour bordered")
-// >>>>>>> 605f76f04a3cd459b9723c573815b33bcfccd46f
+//         .attr("rx", 4)
+//         .attr("ry", 4)
+//         .attr("class", "hour bordered")
+
         .attr("width", gridSize * 3.2)
         .attr("height", gridSize)
         .on('mouseover', tooltip.show)
